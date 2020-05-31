fd8e698eb901fd9bf3e0acf49732893f10704c39
zafran72 <57694121+zafran72@users.noreply.github.com>
2020-05-31 21:04:49 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('storeProfile', function () {
     return view('storeProfile');
 });
 
+Route::get('animal', function () {
+    return view('produsen/animal');
+});
+
 Route::get('detail', function () {
     return view('detail');
 });
@@ -29,5 +33,10 @@ Route::get('ternakku', function () {
     return view('ternakku');
 });
 
-Route::get('login', 'ProdusenController@login');
-Route::get('signup', 'ProdusenController@signup');
+Route::get('/admin/login', 'AdminController@login');
+
+Route::get('/produsen/login', 'ProdusenController@login');
+Route::get('/produsen/signup', 'ProdusenController@signup');
+
+Route::get('/pedagang/login', 'PedagangController@login');
+Route::get('/pedagang/signup', 'PedagangController@signup');
