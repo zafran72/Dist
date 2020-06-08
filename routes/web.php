57b4c7ff9b375ba2635b29dd1f23fc05b2dffb4a
FadhilahSyafa <fadhila18@mhs.unsyiah.ac.id>
2020-06-08 23:57:55 +0700

    Edit Controller

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,8 @@ Route::get('/pedagang/chooseTernak', function () {
 });
 
 
-Route::get('/contact', function () {
-    return view('contact');
+Route::get('/Contact', function () {
+    return view('Contact');
 });
 
 Route::get('/admin/login', 'AdminController@login');
